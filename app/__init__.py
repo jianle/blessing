19d79586822f39cdefcd5098c1eca46366b7cf88
乐健 <jianle@anjuke.com>
2016-05-04 19:25:42 +0800

    add test

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
-from flask import Flask
+#from flask import Flask
+#
+#app = Flask(__name__)
+#app.config.from_object("config")
+#
+#from views.public import *
 
-app = Flask(__name__)
-app.config.from_object("config")
 
-from app import views, models
+def create_app(config_name):
+    # ...
+  from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    return app
