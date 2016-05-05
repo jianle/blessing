a1a9f9753766137c09a617737cc36e2279ff53a7
乐健 <jianle@anjuke.com>
2016-05-05 10:46:00 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,7 @@
-#from flask import Flask
-#
-#app = Flask(__name__)
-#app.config.from_object("config")
-#
-#from views.public import *
+from flask import Flask
 
+app = Flask(__name__)
+app.config.from_object("config")
 
-def create_app(config_name):
-    # ...
-  from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-    return app
+from .main import main
+
