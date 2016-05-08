e8afc5b0e95e3c3b0bbbe48b004645c9d49ae5b9
乐健 <jianle@anjuke.com>
2016-05-08 20:21:54 +0800

    add migration

---
+++ migration.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE `bless`  DEFAULT CHARACTER SET UTF8;
+USE `bless`;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `truename` varchar(255) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `role` int(11) NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `euk` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
+
+insert into user values(1, 'xx', 'xx', 'pwd', 'email', -1, current_timestamp())
+
